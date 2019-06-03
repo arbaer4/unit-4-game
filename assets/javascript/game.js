317acfd4dde56d7394a7baf3cdbb019f4612e2f3
Alexandra Baer <alexbaer@Alexandras-MacBook-Pro.local>
2019-06-03 14:42:10 -0400

    reset function working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,65 @@
 $(document).ready(function() {
+  var random;
 
+        //generate random number for crystals
+        var crystalnum1 = 0;
+        var crystalnum2 = 0;
+        var crystalnum3 = 0;
+        var crystalnum4 = 0;
+      
+      
+      //var for user score
+      var counter = 0;
+      var wins = 0;
+      var losses = 0;
+  
     //on click start the game by generating a random number
-        $("#clicktoplay").on("click", function() {
-    
+    function reset(){
+      console.log("reset called");
+      random = Math.floor(Math.random() * 100) + 19;
+        console.log(random);
+      
+        // place random number in random number div.
+        $("#randomnumber").text(random);
+  
+        
+        //generate random number for crystals
+        crystalnum1 = Math.floor(Math.random()*12)+1;
+        crystalnum2 = Math.floor(Math.random()*12)+1;
+        crystalnum3 = Math.floor(Math.random()*12)+1;
+        crystalnum4 = Math.floor(Math.random()*12)+1;
+      
+      
+      //var for user score
+      counter = 0;
+      $("#userTotal").text(counter);
+      }
+
+
+  $("#clicktoplay").on("click", function() {
+ 
       //generate a random number
-      var random = Math.floor(Math.random() * 100) + 19;
+      random = Math.floor(Math.random() * 100) + 19;
       console.log(random);
     
       // place random number in random number div.
       $("#randomnumber").text(random);
 
+      
       //generate random number for crystals
-      var crystalnum1 = Math.floor(Math.random()*12)+1;
-      var crystalnum2 = Math.floor(Math.random()*12)+1;
-      var crystalnum3 = Math.floor(Math.random()*12)+1;
-      var crystalnum4 = Math.floor(Math.random()*12)+1;
+      crystalnum1 = Math.floor(Math.random()*12)+1;
+      crystalnum2 = Math.floor(Math.random()*12)+1;
+      crystalnum3 = Math.floor(Math.random()*12)+1;
+      crystalnum4 = Math.floor(Math.random()*12)+1;
+    
     
     //var for user score
-    var counter = 0;
-    var wins = 0;
-    var losses = 0;
+    counter = 0;
+    wins = 0;
+    losses = 0;
 
+
+          
 
     // $("#wins").text(wins);
     // $("#losses").text(losses);
@@ -30,14 +69,16 @@ $(document).ready(function() {
             alert("You win!");
             wins++;
             $("#wins").text(wins);
-            console.log(wins);
+            console.log(wins); 
+            reset();
     }
 
     function fail(){
             alert("You lose!!");
             losses++;
             $("#losses").text(losses);
-            console.log(losses);
+            console.log(losses);  
+            reset();    
     }
 
     //incrementing user score by clicking crystals:
